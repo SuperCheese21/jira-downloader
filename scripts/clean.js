7026721cdb651f929d1dcf9928a2c7651b68c31f
SuperCheese21 <ethan.shields21@gmail.com>
2018-08-09 10:57:51 -0700

    Excluded placeholder file from being deleted, added readline

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,19 +1,44 @@
 const fs = require('fs');
+const readline = require('readline');
 
-const PATH = './output/attachments';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+rl.question('Do you want to delete everything in the attachments folder? (y/n): ', answer => {
+    if (answer[0].toLowerCase() ===  'y') {
+        cleanDirectory('./output/attachments');
+    }
+    rl.close();
+})
+
+/**
+ * Recursive function that removes all files and directories inside a directory
+ * @param  {String} path Path to directory
+ */
 function cleanDirectory(path) {
+    // Loop through each item in directory
     fs.readdirSync(path).forEach(item => {
         const fullPath = path + '/' + item;
-        if (fs.lstatSync(fullPath).isDirectory()) {
-            cleanDirectory(fullPath);
-            fs.rmdirSync(fullPath);
-            console.log('Removed ' + item);
-        } else {
-            fs.unlinkSync(fullPath);
-            console.log(' Removed ' + item);
+
+        // Base case: item is a file
+        if (fs.lstatSync(fullPath).isFile()) {
+            // Remove file if not .placeholder
+            if (item[0] !== '.') {
+                fs.unlinkSync(fullPath);
+                console.log(' Removed ' + item);
+            }
+            return;
         }
+
+        // Call self if item is a directory
+        cleanDirectory(fullPath);
+
+        // Remove directory after it is emptied
+        fs.rmdirSync(fullPath);
+        console.log('Removed ' + item);
     });
 }
 
-cleanDirectory(PATH);
+module.exports = cleanDirectory;
